507c7a3aeea55a1da1121bae697a308eb9336d92
Kouki Murakami <m.kouki.0928@gmail.com>
2022-11-17 16:54:43 +0900

    add 課題1

---
+++ src/1/fixedPointNumber.ts
@@ -0,0 +1,34 @@
+const convertNumberToBinary = (num: number, digits: number) => {
+  const numStr = num.toString();
+  const integersAndDecimals = numStr.split('.');
+  const integers = Number(integersAndDecimals[0]);
+  const decimals = Number(`0.${integersAndDecimals[1]}`);
+  const intBinary = convertIntegerToBinary(integers);
+  const decBinary = convertDecimalToBinary(decimals, digits);
+  return `0b${intBinary}.${decBinary}`;
+};
+
+const convertIntegerToBinary = (num: number) => {
+  let n = num;
+  const binary = [];
+  while (n !== 0) {
+    binary.push(Math.floor(n % 2));
+    n = Math.floor(n / 2);
+  }
+  return binary.reverse().join('');
+};
+
+const convertDecimalToBinary = (num: number, digits: number) => {
+  let n = num;
+  const binary = [];
+  for (let i = 0; i < digits; i++) {
+    n = n * 2;
+    binary.push(Math.floor(n));
+    if (Math.floor(n) === 1) n = n - 1;
+  }
+  return binary.join('');
+};
+
+console.log(convertNumberToBinary(12.345, 10));
+console.log(convertNumberToBinary(57.39, 10));
+console.log(convertNumberToBinary(113.06, 10));
